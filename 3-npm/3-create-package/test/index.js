3679d02ed80d5f53f9751647b3602b91af043c0b
kinyeramos2021 <kinyeramo@gmail.com>
2021-10-01 11:14:09 +0300

    Node js getting started

---
+++ 3-npm/3-create-package/test/index.js
@@ -0,0 +1,11 @@
+const print = require('kinyeramos2021-frame-print');
+
+print('Hello NPM!');
+
+/*
+Expected Output:
+
+**********
+Hello NPM!
+**********
+*/
---
+++ 4-modules/1-define-use/3-wrapper.js
@@ -0,0 +1,11 @@
+
+//function (exports,module, require, __filename, __dirname) {
+//  let g= 1;
+
+//  console.log(arguments);
+
+  exports.a = 42;
+  module.exports.b = 39;
+
+//  return module.exports;
+//}(module.exports)
